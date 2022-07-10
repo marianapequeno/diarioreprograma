2dff0e60735b26df8c594692ce9ef378b6dd772e
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-07-10 16:50:35 -0300

    commit inicial

---
+++ src/routes/pages/Portifolio/Portifolio.jsx
@@ -0,0 +1,15 @@
+import Header from "../../componentes/Header/Header"
+import image from "../../../assets/project.svg"
+
+function Portifolio() {
+  return (
+    <Header 
+      image={image} 
+      description="Ilustração Projeto"
+    >
+      Meus Projetos
+    </Header>
+  )
+}
+
+export default Portifolio
